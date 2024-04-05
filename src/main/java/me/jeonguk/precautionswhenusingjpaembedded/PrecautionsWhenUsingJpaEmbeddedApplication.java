ce054252a1a6de5678cff9c56626885fa6e27901
JeongUk Park <dnr1105@gmail.com>
2024-04-05 23:55:53 +0900

    프로젝트 생성

---
+++ src/main/java/me/jeonguk/precautionswhenusingjpaembedded/PrecautionsWhenUsingJpaEmbeddedApplication.java
@@ -0,0 +1,13 @@
+package me.jeonguk.precautionswhenusingjpaembedded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecautionsWhenUsingJpaEmbeddedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrecautionsWhenUsingJpaEmbeddedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jeonguk/precautionswhenusingjpaembedded/PrecautionsWhenUsingJpaEmbeddedApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jeonguk.precautionswhenusingjpaembedded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecautionsWhenUsingJpaEmbeddedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
